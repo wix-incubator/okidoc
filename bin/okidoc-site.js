2bab3bfcf5345eb7720a2ba1eeafda7668663188
Bohdan Lyzanets <bohdanl@wix.com>
2018-01-25 16:09:45 +0200

    copy site build (`okidoc/site/public`)  to `PROJECT_ROOT/site` after successful build

--- bin/okidoc-site.js
+++ bin/okidoc-site.js
@@ -2,14 +2,35 @@
 
 const path = require('path');
 const spawn = require('cross-spawn');
+const copy = require('ncp').ncp;
 
 // NOTE: ignore first 2 arguments (node and path of this file)
 const [command, siteYamlPath] = process.argv.slice(2);
 
-// TODO: check `command`
+const ALLOWED_COMMANDS = ['build', 'develop'];
+const SITE_PATH = path.join(__dirname, '../site');
+// TODO: add ability to configure SITE_DIST_PATH
+const SITE_DIST_PATH = path.resolve('./site');
+const RUN_EXAMPLE = `okidoc-site ${ALLOWED_COMMANDS[0]} ./path/to/site.yml`;
 
-spawn('npm', ['run', command || 'develop'], {
-  cwd: path.join(__dirname, '../site'),
+if (!command || !ALLOWED_COMMANDS.includes(command)) {
+  throw new Error(`
+    'command' should be one of ${JSON.stringify(ALLOWED_COMMANDS)}.
+    example:
+    '> ${RUN_EXAMPLE}'
+  `);
+}
+
+if (!siteYamlPath) {
+  throw new Error(`
+    'okidoc-site' config parameter not provided
+    example:
+    '> ${RUN_EXAMPLE}'
+  `);
+}
+
+const site = spawn('npm', ['run', command], {
+  cwd: SITE_PATH,
   stdio: 'inherit',
   env: {
     ...process.env,
@@ -17,3 +38,18 @@ spawn('npm', ['run', command || 'develop'], {
     SITE_YAML_PATH: path.resolve(siteYamlPath),
   },
 });
+
+if (command === 'build') {
+  site.on('close', code => {
+    // NOTE: only success build should be copied
+    if (code !== 0) {
+      return;
+    }
+
+    copy(path.join(SITE_PATH, 'public'), SITE_DIST_PATH, err => {
+      if (!err) {
+        console.log(`open '${SITE_DIST_PATH}' to browse site build`);
+      }
+    });
+  });
+}
