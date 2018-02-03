96cf5010b169024a226bb4b14b0586fd05c3229d
Bohdan Lyzanets <bohdanl@wix.com>
2018-02-04 00:07:51 +0200

    Updates

---
+++ path----08841ab765323d812edc.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{114:function(t,a){t.exports={data:{site:{siteMetadata:{title:"okidoc"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----08841ab765323d812edc.js.map
---
+++ path----29930c717fba623a43bf.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{114:function(t,e){t.exports={data:{site:{siteMetadata:{title:"video-player.js"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----29930c717fba623a43bf.js.map
