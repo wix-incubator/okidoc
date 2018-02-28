9af1e47b45077fea267d05bd3a7b72587bbc84be
Bohdan Lyzanets <bohdanl@wix.com>
2018-02-28 14:37:07 +0200

    [ci] update `scripts/npm-release.js` to handle `dist-tags` other than `latest`

--- scripts/npm-release.js
+++ scripts/npm-release.js
@@ -7,54 +7,56 @@ if (!process.env.IS_BUILD_AGENT) {
 
 const util = require('util');
 const path = require('path');
-const semver = require('semver');
 const exec = util.promisify(require('child_process').exec);
 const writeFile = util.promisify(require('fs').writeFile);
 
-const ZERO_VERSION = '0.0.0';
-
-function getPublishedVersion() {
-  return exec('npm view okidoc version --registry=https://registry.npmjs.org/')
-    .then(result => result.stdout && result.stdout.trim())
+function getPublishedVersions() {
+  return exec('npm view okidoc versions --registry=https://registry.npmjs.org/')
+    .then(
+      result =>
+        result.stdout && JSON.parse(result.stdout.trim().replace(/'/g, '"')),
+    )
     .catch(e => {
       if (e.stderr.startsWith('npm ERR! code E404')) {
         console.log('ERROR: package not found. Possibly not published yet', e);
-        return ZERO_VERSION;
+        return [];
       }
 
       console.log('ERROR: Unable to get published version from npmjs.org', e);
     });
 }
 
-function shouldPublish(localVersion, publishedVersion) {
-  return (
-    publishedVersion === ZERO_VERSION ||
-    (!!publishedVersion && semver.gt(localVersion, publishedVersion))
-  );
+function shouldPublish(localVersion, publishedVersions) {
+  return !publishedVersions.length || !publishedVersions.includes(localVersion);
 }
 
 function stringify(data) {
   return JSON.stringify(data, null, 2);
 }
 
-getPublishedVersion()
-  .then(publishedVersion => {
+getPublishedVersions()
+  .then(publishedVersions => {
     const packageJSONPath = path.resolve(__dirname, '..', 'package.json');
     const packageJSON = require(packageJSONPath);
     const lernaJSON = require('../lerna.json');
 
-    if (shouldPublish(lernaJSON.version, publishedVersion)) {
+    if (
+      publishedVersions &&
+      shouldPublish(lernaJSON.version, publishedVersions)
+    ) {
       // NOTE: use `private: false` to allow publish to npm
       // https://github.com/wix-private/wix-ci/blob/master/ci-scripts/wix-agent-scripts/src/npmBuild.sh#L72
       return writeFile(
         packageJSONPath,
         stringify(Object.assign({}, packageJSON, { private: false })),
       ).then(() => {
-        if (publishedVersion !== ZERO_VERSION) {
+        if (publishedVersions.length) {
           console.log(
             `Version set in lerna.json ${
               lernaJSON.version
-            } is newer than published ${publishedVersion}`,
+            } is not published yet. Last published version is ${
+              publishedVersions[publishedVersions.length - 1]
+            }`,
           );
         }
 
@@ -62,7 +64,7 @@ getPublishedVersion()
       });
     } else {
       console.log(`Package will not be published`);
-      if (publishedVersion) {
+      if (publishedVersions) {
         console.log(
           `version ${lernaJSON.version} set in lerna.json is already published`,
         );
