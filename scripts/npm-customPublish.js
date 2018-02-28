d841d9869a24ca7c3358ec1540c4b2b941f9875d
Bohdan Lyzanets <bohdanl@wix.com>
2018-02-28 14:01:02 +0200

    [ci] update `customPublish` with `lerna exec` instead of `lerna publish`

--- scripts/npm-customPublish.js
+++ scripts/npm-customPublish.js
@@ -1,44 +1,26 @@
 // customPublish script for ci
 // before customPublish, packages should be prepared by npm run preparePublish
-//
-// if (!process.env.IS_BUILD_AGENT) {
-//   console.log(
-//     "Package will not be published because we're not running in a CI build agent",
-//   );
-//   return process.exit(0);
-// }
 
-console.log('disable publish before fix lerna + ci + git');
-return process.exit(0);
+if (!process.env.IS_BUILD_AGENT) {
+  console.log(
+    "Package will not be published because we're not running in a CI build agent",
+  );
+  return process.exit(0);
+}
 
 const lernaCLI = require('lerna/lib/cli');
-const PublishCommand = require('lerna/lib/commands/PublishCommand').default;
 const semver = require('semver');
 
 const lernaJSON = require('../lerna');
 
-// NOTE: new version is in packages and git, but not in npm repo. Without `--force-publish` lerna will skip publishing
+// NOTE: lerna publish is not supported on CI due to https://github.com/lerna/lerna/issues/627
 
 // https://github.com/lerna/lerna/blob/2.x/bin/lerna.js
-// https://github.com/lerna/lerna#publish
-const argv = [
-  'publish',
-  '--skip-git',
-  '--force-publish',
-  '--repo-version',
-  lernaJSON.version,
-  '--yes',
-];
+// https://github.com/lerna/lerna#exec
+const argv = ['exec', '--concurrency', '1', '--', 'npm', 'publish'];
 
 if (semver.prerelease(lernaJSON.version) !== null) {
-  argv.push('--npm-tag=next');
+  argv.push('--tag=next');
 }
-
-// git is not required for CI npm publish
-// prevent `lerna` git validation
-// https://github.com/lerna/lerna/blob/2.x/src/Command.js#L240-L245
-Object.defineProperty(PublishCommand.prototype, 'requiresGit', {
-  get: () => false,
-});
 
 lernaCLI().parse(argv);
