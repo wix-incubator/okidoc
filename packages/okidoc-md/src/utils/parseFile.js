5bb49532517634387e08900efd54287382d77d80
Bohdan Lyzanets <bohdanl@wix.com>
2018-04-02 14:26:10 +0300

    add readable errors for file parsing logic

--- packages/okidoc-md/src/utils/parseFile.js
+++ packages/okidoc-md/src/utils/parseFile.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
+import path from 'path';
 import parseSource from './parseSource';
 
 function parseFile(filePath) {
   const fileSource = fs.readFileSync(filePath).toString();
 
-  return parseSource(fileSource);
+  try {
+    return parseSource(fileSource);
+  } catch (error) {
+    error.message = `${path.relative(process.cwd(), filePath)}: ${
+      error.message
+    }`;
+    throw error;
+  }
 }
 
 export default parseFile;
