a4b47a0d86b3af522cafd1881aadac4074ed1532
Bohdan Lyzanets <bohdanl@wix.com>
2018-05-31 17:34:29 +0300

    fix(okidoc-site): fix issue with headings with same name on one page
    if different md files included to one page via `frontmatter` `include` has heading with same name, heading links could be invalid.
    This change, should fix it

--- packages/okidoc-site/site/src/utils/getPageHeadingsAndHtmlAst.js
+++ packages/okidoc-site/site/src/utils/getPageHeadingsAndHtmlAst.js
@@ -1,3 +1,28 @@
+import visit from 'unist-util-visit';
+import Slugger from 'github-slugger';
+
+const HEADING_TAG_PATTERN = /^h[1-6]$/;
+
+function fixHeadingLinks(htmlAst) {
+  const slugger = new Slugger();
+
+  visit(htmlAst, 'element', element => {
+    if (HEADING_TAG_PATTERN.test(element.tagName)) {
+      const id = element.properties.id;
+      const slug = slugger.slug(id);
+
+      if (id !== slug) {
+        element.properties.id = slug;
+        element.children.forEach(child => {
+          if (child.type === 'element' && child.tagName === 'a') {
+            child.properties.href = `#${slug}`;
+          }
+        });
+      }
+    }
+  });
+}
+
 function getPageHeadingsAndHtmlAst(page) {
   const headings = [...page.headings];
   const htmlAst = {
@@ -31,6 +56,9 @@ function getPageHeadingsAndHtmlAst(page) {
       headings.push(...childMarkdownRemark.headings);
       htmlAst.children.push(...childMarkdownRemark.htmlAst.children);
     });
+
+    // NOTE: fix heading link after joining md includes
+    fixHeadingLinks(htmlAst);
   }
 
   return {
